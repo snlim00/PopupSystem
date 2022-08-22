c20a6adade5c51867c2aa47b8a0e5e6a2662a525
snlim00 <87693520+snlim01@users.noreply.github.com>
2022-08-22 23:55:08 +0900

    - GameStartPopup의 Speed설정 개발 중

--- Assets/Scripts/SongSelect/GameStartPopup.cs
+++ Assets/Scripts/SongSelect/GameStartPopup.cs
@@ -6,22 +6,31 @@ using TMPro;
 
 public class GameStartPopup : Popup
 {
+    #region UI 요소들
     [SerializeField] private Image jacket;
     [SerializeField] private TMP_Text musicNameText;
     [SerializeField] private TMP_Text artistText;
 
-    [SerializeField] private TMP_Text SpeedValue;
-    [SerializeField] private Button SpdLeftButton;
-    [SerializeField] private Button SpdRightButton;
+    [SerializeField] private TMP_Text speedValue;
+    [SerializeField] private Button spdLeftButton;
+    [SerializeField] private Button spdRightButton;
 
-    [SerializeField] private TMP_Text DifficultyValue;
-    [SerializeField] private Button DifLeftButton;
-    [SerializeField] private Button DifRightButton;
+    [SerializeField] private TMP_Text difficultyValue;
+    [SerializeField] private Button difLeftButton;
+    [SerializeField] private Button difRightButton;
+    #endregion
 
+    #region 해당 악곡의 정보
     private string levelName;
     private List<string> difficulties = new List<string>();
 
     private int selectedDifIndex = 0;
+    #endregion
+
+    public float selectedSpeed = 1f; //해당 값은 유저 데이터의 speed값으로 변경되어야 함
+
+    private const float minSpeed = 0.1f;
+    private const float maxSpeed = 1f;
 
     public void OpenGameStartPopup(in MusicListObject musicData)
     {
@@ -34,5 +43,17 @@ public class GameStartPopup : Popup
         //유저 데이터에서 설정된 속도 값을 가져와서 적용시키기.
 
         base.OpenPopup();
+    }
+
+    protected override void _Awake()
+    {
+        base._Awake();
+
+        InitSpeedValue();
+    }
+
+    private void InitSpeedValue()
+    {
+        speedValue.text = selectedSpeed.ToString();
     }
 }
